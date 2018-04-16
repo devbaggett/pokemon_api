d2e1cf445abcda0a31aa29b21007ee4a997e299e
devbaggett <devabaggett@gmail.com>
2018-04-16 13:41:31 -0700

    fixes

--- AngularApp/src/app/http.service.ts
+++ AngularApp/src/app/http.service.ts
@@ -2,44 +2,35 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+
+var bulbasaur;
+var bulb_ability_1, bulb_ability_2;
+// var poke_arr = [];
+
 @Injectable()
 export class HttpService {
   	constructor(private _http: HttpClient) {
-  		// this.getPokemon();
-  		this.getAll();
+  		this.getPokemon();
+  		// this.getAll();
   	}
-  	const bulb1;
-  	// getPokemon(){
-  	// 	let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
-  	// 	bulbasaur.subscribe(data => {
-  	// 		console.log('Got our pokemon!', data);
-  	// 		let abilities = data['abilities'];
-  	// 		for (let i in abilities){
-  	// 			console.log(abilities[i].ability.name);
-  	// 		}
-  	// 	})
-  	// }
-  	getAll(){
-  		let new_arr = [];
-  		let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+	getPokemon(){
+  		bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
   		bulbasaur.subscribe(data => {
-  			let abilities = data['abilities'];
-  			let bulb1 = abilities[0].ability.name;
-  			let bulb2 = abilities[1].ability.name;
-  		for (var i = 2; i < 25; i++){
-  			let poke = this._http.get('https://pokeapi.co/api/v2/pokemon/' + i + '/');
-  			poke.subscribe(data => {
-  				if (bulb1 == data['abilities'][0].ability.name ||
-  					bulb2 == data['abilities'][0].ability.name ||
-  					bulb1 == data['abilities'][1].ability.name ||
-  					bulb2 == data['abilities'][1].ability.name ||){
-  						console.log(data.name + "shares the same ability!");
-  				}
-  				else{
-  					console.log("Bulbasaur doesn't share the same abilities!")
-  				}
-  				
-  			})
-  		}
+			console.log('Got our pokemon!', data);
+			bulb_ability_1 = data['abilities'][0].ability.name;
+			bulb_ability_2 = data['abilities'][1].ability.name;
+			console.log(bulb_ability_1);
+			console.log(bulb_ability_2);
+  		})
   	}
+  	// getAll(){
+  	// 	// let poke = this._http.get('https://pokeapi.co/api/v2/pokemon/' + i + '/');
+  	// 	for (var i = 2; i < 25; i++){
+  	// 		let poke = this._http.get('https://pokeapi.co/api/v2/pokemon/' + i + '/');
+  	// 		poke.subscribe(data => {
+  	// 			console.log(data);
+  	// 		})
+  	// 	}
+  		
+  	// }
 }
